abcc3fab445e6c6b775f0e9a5e4d5ce5d4fa7cb2
Veniamin Zinevych <mitchsvik@gmail.com>
2016-06-27 15:10:50 +0300

    version 0.0.4
    + Login manager with cookies support;
    + Logout function;

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from flask import render_template, redirect, flash
-from app import app
+from flask import render_template, redirect, flash, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'nickname' : 'Mitchsvik'}
+	user = g.user
 	posts = [
 		{
 			'author' : {'nickname' : 'Mitchsvik'},
@@ -16,17 +19,52 @@ def index():
 			'body' : 'another text'
 		}
 	]
-	return render_template('index.html', title = user['nickname'],
+	return render_template('index.html', title = 'Home',
                            user = user, posts = posts)
 
+@app.route('/registration', methods = {'GET', 'POST'})
+def registration():
+    pass
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
 @app.route('/login', methods = {'GET', 'POST'})
-def login():
+def login():                               
+    if g.user is not None and g.user.is_authenticated: 
+        return redirect(url_for('index'))
+    
     form = LoginForm()
     
     if form.validate_on_submit():
-        flash('Login = "' + form.login.data + '", password = "' + form.password.data + 
-              '", remember_me = ' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return try_login(login = form.login.data, password = form.password.data)
     
     return render_template('login.html', title = 'Sign In',
                            form = form)
+
+def try_login(login = None, password = None):
+    user = User.query.filter_by(nickname = login).first()
+    if user is not None:
+        if user.password == password:
+            remember_me = False
+            if 'remember_me' in session:
+                remember_me = session['remember_me']
+                session.pop('remember_me', None)
+            
+            print user
+            login_user(user = user, remember = remember_me)
+            return redirect(request.args.get('next') or url_for('index'))
+        
+    flash('Invalid login. Please try again')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
