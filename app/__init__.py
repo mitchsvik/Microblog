acc04a326a753d269c452275a3a47ae3328586b2
Veniamin Zinevych <mitchsvik@gmail.com>
2016-07-11 21:32:21 +0300

    version 0.1.2
    + Moment.js for date time show;
    * Underscored post template;

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 
 lm = LoginManager()
